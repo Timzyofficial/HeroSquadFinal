1031b5215cb4712e5a6807b51967c7f0246e90f1
Timothy Maina Karanja <tkaranja@safaricom.co.ke>
2023-08-23 19:30:50 +0300

    implemented the hero class

---
+++ src/main/java/org/example/models/Hero.java
@@ -0,0 +1,60 @@
+package org.example.models;
+
+public class Hero {
+    private static int nextId = 1;
+    private String name;
+    private int age;
+
+    public Hero(String name, int age, String power, String weakness) {
+        this.name = name;
+        this.age = age;
+        this.power = power;
+        this.weakness = weakness;
+        id = nextId ++;
+    }
+
+    private String power;
+    private String weakness;
+
+    private int id;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPower() {
+        return power;
+    }
+
+    public void setPower(String power) {
+        this.power = power;
+    }
+
+    public String getWeakness() {
+        return weakness;
+    }
+
+    public void setWeakness(String weakness) {
+        this.weakness = weakness;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
